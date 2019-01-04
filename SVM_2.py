43ea1edf146914aa1f75b4f938fcd965fa4660cf
Peaceholic <enflwodn@naver.com>
2019-01-04 10:19:57 +0900

    Ensemble 3

--- SVM_2.py
+++ SVM_2.py
@@ -1,8 +1,3 @@
-from IPython.display import display
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import mglearn
 from sklearn.svm import SVC
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
