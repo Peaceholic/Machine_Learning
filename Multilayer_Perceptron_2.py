fb0a6bb876910b74d37658a5b86b6b6a53d779c6
Peaceholic <enflwodn@naver.com>
2019-01-04 22:40:00 +0900

    Neural Network

---
+++ Multilayer_Perceptron_2.py
@@ -0,0 +1,36 @@
+from IPython.display import display
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+cancer = load_breast_cancer()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, random_state=0
+)
+
+# standard normal distribution
+mean_on_train = X_train.mean(axis=0)
+std_on_train = X_train.std(axis=0)
+
+X_train_scaled = (X_train - mean_on_train)/std_on_train
+X_test_scaled = (X_test - mean_on_train)/std_on_train
+
+mlp = MLPClassifier(max_iter=1000, alpha=1, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+print("Train Set Score: {:.2f}".format(mlp.score(X_train_scaled, y_train)))
+print("Test Set Score: {:.2f}".format(mlp.score(X_test_scaled, y_test)))
+
+plt.figure(figsize=(20,5))
+plt.imshow(mlp.coefs_[0], interpolation='none', cmap='viridis')
+plt.yticks(range(30), cancer.feature_names)
+plt.xlabel("Hidden Units")
+plt.ylabel("Input Features")
+plt.colorbar()
+
+plt.show()
