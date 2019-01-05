670298c924588a71265639f2b18b14f1011074de
Peaceholic <enflwodn@naver.com>
2019-01-05 14:55:26 +0900

    Feature Engineering

---
+++ One_Hot_Encoding.py
@@ -0,0 +1,24 @@
+from IPython.display import display
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+import os
+
+data = pd.read_csv(
+    os.path.join(mglearn.datasets.DATA_PATH, "adult.data"),
+    header=None, index_col=False,
+    names=['age', 'workclass', 'fnlwgt', 'education', 'education-num',
+           'martial-status', 'occupation', 'relationship', 'race', 'gender',
+           'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+           'income']
+)
+
+data = data[['age', 'workclass','education', 'gender', 'hours-per-week',
+             'occupation', 'income']]
+
+display(data.head())
+
+print("Original Features:\n", list(data.columns), "\n")
+data_dummies = pd.get_dummies(data)
+print("After Features:\n", list(data_dummies.columns))
